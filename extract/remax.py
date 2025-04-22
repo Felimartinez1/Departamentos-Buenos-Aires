761aac4831185810ebdf7675ff6b724085febf65
Antonio Mestroni <tonymestroni@gmail.com>
2025-04-21 22:59:15 -0300

    Update remax.py

--- extract/remax.py
+++ extract/remax.py
@@ -1,1 +1,79 @@
 import time
+import requests
+from bs4 import BeautifulSoup as bs
+import random
+import time
+import pandas as pd
+import numpy as np
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import undetected_chromedriver as uc
+import re
+
+
+browser = uc.Chrome()
+
+
+url_base = "https://www.remax.com.ar/listings/"
+max_paginas = 65
+links_lista = []
+
+def limpiar_titulo_para_link(titulo):
+    titulo = titulo.lower()
+    titulo = re.sub(r"[^\w\s-]", "", titulo)  
+    titulo = re.sub(r"\s+", "-", titulo.strip()) 
+    return titulo
+
+
+pagina_actual = 1 
+
+
+x = 1 
+
+while x <= max_paginas:
+    if x == pagina_actual:
+        url = f"{url_base}rent?page={x}&pageSize=24&sort=-createdAt&in:operationId=2&in:eStageId=0,1,2,3,4&in:typeId=1,2,3,4,5,6,7,8&locations=in:CF@%3Cb%3ECapital%3C%2Fb%3E%20%3Cb%3EFederal%3C%2Fb%3E::::::&landingPath=&filterCount=1&viewMode=listViewMode"
+        browser.get(url)
+        time.sleep(random.uniform(8, 11))
+
+        
+        body = browser.find_element("tag name", "body")
+        body.send_keys(Keys.PAGE_DOWN)
+        time.sleep(random.randint(2, 4))
+
+        html = browser.page_source
+        soup = bs(html, 'lxml')
+
+       
+        propiedades = soup.find_all("div", class_="card-remax__container")
+
+        for propiedad in propiedades:
+            titulo_tag = propiedad.find("p", class_="card__description")
+            if titulo_tag:
+                titulo = titulo_tag.get_text(strip=True)
+                slug = limpiar_titulo_para_link(titulo)
+                link = f"https://www.remax.com.ar/listings/{slug}"
+                links_lista.append({
+                    "Titulo": titulo,
+                    "Link": link
+                })
+
+        print(f"Página {x} procesada")
+
+        
+        if x == max_paginas:
+            print("Se alcanzó la última página, finalizando ejecución.")
+            break
+
+        
+        x = x + 1
+    
+    else:
+        x = x + 1
+
+
+df = pd.DataFrame(links_lista)
+df.to_csv("LinksPropiedades.csv", index=False, encoding="utf-8-sig", sep=";")
+print("Títulos y links generados correctamente.")
+browser.quit()
